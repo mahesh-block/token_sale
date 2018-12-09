0790002660be2f65ea7541185714a7c1940a3e41
mahesh-block <maheshbabu.nlp@gmail.com>
2018-12-08 23:33:25 -0500

    my changes

--- test/DappToken.js
+++ test/DappToken.js
@@ -2,20 +2,25 @@ var DappToken = artifacts.require("./DappToken.sol");
 
 // testing the total supply
 
-var totalInstance;
-beforeEach("deploying the contract", async () => {
+let totalInstance;
+beforeEach(async () => {
   totalInstance = await DappToken.deployed();
 });
 
-it("sets the total supply upon deployment", async () => {
-  const instance = await totalInstance.totalSupply();
-  assert.equal(
-    instance.toNumber(),
-    1000000,
-    "sets the total supply to 1,000,000"
-  );
-  it("allocating initial supply to admin", async () => {
-    const balance = await totalInstance.balanceOf(accounts[0]);
-    assert.equal(balance.toNumber(), 1000000, "allocating the balance");
+describe("contracts", () => {
+  it("sets the total supply upon deployment", async () => {
+    const instance = await totalInstance.totalSupply();
+    assert.equal(
+      instance.toNumber(),
+      1000000,
+      "sets the total supply to 1,000,000"
+    );
+    it("allocating initial supply to admin", async () => {
+      const balance = await totalInstance.methods.balanceOf().call({
+        from: accounts[0]
+      });
+      console.log("hi Mahesh");
+      assert.equal(balance.toNumber(), 1000000, "allocating the balance");
+    });
   });
 });
