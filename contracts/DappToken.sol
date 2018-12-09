a11ab5e885a9c93b1a90e7642a7970dfaecae9ac
mahesh-block <maheshbabu.nlp@gmail.com>
2018-12-09 03:46:11 -0500

    my changes

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -4,11 +4,58 @@ contract DappToken {
 
         // variable declaration
         uint256 public totalSupply;
-        mapping (address => uint256) public balanceOf;
+        string public name = "DApp Token";
+        string public symbol = "DAPP";
+        string public standard = "DApp Token v1.0";
 
+        //event triggers when tokens are transferred
+        event Transfer(
+            address indexed _from,
+            address indexed _to,
+            uint256 _value
+            );
+        event Approval(
+            address indexed _owner,
+            address indexed _spender,
+            uint256 _value
+            );
+        mapping (address => uint256) public balanceOf;
+        //defining variable how much owner allowed to spend
+        mapping (address => mapping (address => uint256) )public allowance;
+        //constructor function with initial supply
         function DappToken(uint256 _initialSupply) public{
+            balanceOf[msg.sender] = _initialSupply;
             totalSupply = _initialSupply;
         }
 
-        
+        //Transfer function
+        function transfer(address _to,uint256 _value) public returns (bool success) {
+            //exception if account doesn't have enough value
+            require(balanceOf[msg.sender] >= _value);
+            balanceOf[msg.sender] -= _value;
+            balanceOf[_to] += _value;
+            Transfer(msg.sender, _to, _value);
+
+            return true;
+        }
+        // approving spender to spend your money
+        function approve(address _spender,uint256 _value) public returns (bool success) {
+            allowance[msg.sender][_spender]=_value;
+            Approval(msg.sender, _spender, _value);
+            return true;
+        }
+        //transfer from
+        function transferFrom(address _from,address _to,uint256 _value) public returns (bool success) {
+            //owner has enough tokens
+            require(_value <= balanceOf[_from]);
+            require(_value <= allowance[_from][msg.sender]);
+            balanceOf[_from] -= _value;
+            balanceOf[_to] += _value;
+
+            allowance[_from][msg.sender] -=_value;
+            Transfer(_from, _to, _value);
+            //change balance
+
+            return true;
+        }
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -1,26 +1,129 @@
 var DappToken = artifacts.require("./DappToken.sol");
 
 // testing the total supply
-
-let totalInstance;
-beforeEach(async () => {
-  totalInstance = await DappToken.deployed();
+let tokenInstance;
+let accounts;
+beforeEach("sets the supply", async () => {
+  tokenInstance = await DappToken.deployed();
+  accounts = web3.eth.accounts;
 });
 
-describe("contracts", () => {
-  it("sets the total supply upon deployment", async () => {
-    const instance = await totalInstance.totalSupply();
-    assert.equal(
-      instance.toNumber(),
-      1000000,
-      "sets the total supply to 1,000,000"
+it("sets the initial supply upon deployment", async () => {
+  const instance = await tokenInstance.totalSupply();
+  assert.equal(instance.toNumber(), 1000000);
+});
+it("check admin balance", async () => {
+  const balance = await tokenInstance.balanceOf(accounts[0]);
+  assert.equal(balance.toNumber(), 1000000);
+});
+
+it("transfers token ownership", async () => {
+  try {
+    const instance = await tokenInstance.transfer.call(
+      accounts[1],
+      999999999999999
     );
-    it("allocating initial supply to admin", async () => {
-      const balance = await totalInstance.methods.balanceOf().call({
-        from: accounts[0]
-      });
-      console.log("hi Mahesh");
-      assert.equal(balance.toNumber(), 1000000, "allocating the balance");
-    });
+  } catch (err) {
+    assert(
+      err.message.indexOf("revert") >= 0,
+      "error message must contain revert"
+    );
+  }
+});
+it("adds the amount to receiving amount", async () => {
+  const instance = await tokenInstance.transfer(accounts[1], 250000, {
+    from: accounts[0]
   });
+  assert.equal(instance.logs.length, 1, "triggers one event");
+  assert.equal(
+    instance.logs[0].event,
+    "Transfer",
+    'Should be "Transfer" event'
+  );
+  assert.equal(
+    instance.logs[0].args._from,
+    accounts[0],
+    "logs the account the tokens are transferred from"
+  );
+  assert.equal(
+    instance.logs[0].args._to,
+    accounts[1],
+    "logs the account the tokens are transferred to"
+  );
+  assert.equal(
+    instance.logs[0].args._value,
+    250000,
+    "logs the transferred amount"
+  );
+  const balance = await tokenInstance.balanceOf(accounts[1]);
+  assert.equal(balance.toNumber(), 250000);
+});
+
+it("approves tokens for delegated transfer", async () => {
+  // const instance = await tokenInstance.approve.call(accounts[1], 100);
+  // assert.equal(instance, true, "it returns true");
+  const instance = await tokenInstance.approve(accounts[1], 100);
+  assert.equal(instance.logs.length, 1, "triggers one event");
+  assert.equal(
+    instance.logs[0].event,
+    "Approval",
+    'Should be "Approval" event'
+  );
+  assert.equal(
+    instance.logs[0].args._owner,
+    accounts[0],
+    "logs the account the tokens are authorized by"
+  );
+  assert.equal(
+    instance.logs[0].args._spender,
+    accounts[1],
+    "logs the account the tokens are authorized to"
+  );
+  assert.equal(
+    instance.logs[0].args._value,
+    100,
+    "logs the transferred amount"
+  );
+  const allow = await tokenInstance.allowance(accounts[0], accounts[1]);
+  assert.equal(allow, 100, "stores the allownace for delegated transfer");
+});
+
+it("handles delegated token transfers", async () => {
+  fromAccount = accounts[2];
+  toAccount = accounts[3];
+  spendingAccount = accounts[4];
+  //transfer some tokens
+  const instance = await tokenInstance.transfer(fromAccount, 100, {
+    from: accounts[0]
+  });
+  const allow_spend = await tokenInstance.approve(spendingAccount, 10, {
+    from: fromAccount
+  });
+  try {
+    const large_spend = await tokenInstance.transferFrom(
+      fromAccount,
+      toAccount,
+      9999,
+      { from: spendingAccount }
+    );
+  } catch (err) {
+    assert(
+      err.message.indexOf("revert") >= 0,
+      "can't transfer value larger than balance"
+    );
+  }
+  //try transferring something larger than approved amount
+  try {
+    const amount = await tokenInstance.transferFrom(
+      fromAccount,
+      toAccount,
+      20,
+      { from: spendingAccount }
+    );
+  } catch (err) {
+    assert(
+      err.message.indexOf("revert") >= 0,
+      "can't transfer value larger than approved amount"
+    );
+  }
 });
